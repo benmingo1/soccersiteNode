9533797749f4137311241551594a96f775391aa6
benmingo1 <bemi7819@colorado.edu>
2017-07-25 07:12:06 -0400

    add to github

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+
+var app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+  res.sendfile(__dirname +'/views/index.html');
+})
+
+app.get('/about', function(req, res){
+  res.send('this is the about page');
+})
+
+app.get('/roster', function(req, res){
+  res.send('this is the roster page');
+})
+
+app.get('/contact', function(req, res){
+  res.send('this is the conact page');
+})
+
+app.use(express.static('public'));
+
+app.listen(3000);
